da20b50b7ff0b9c194e4d51845ac5b0de0062fcf
Dakshbarad <daksh16121999@gmail.com>
2021-07-26 22:52:12 +0530

    Modifications made in all files

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-//First Comment
-//Second Comment
 
-// Working on my side
+let data = ['4', '6', '0', '4', '9', '2', '0', '8', '4', '5', '1', '6', '5', '6', '8'];
+let dataLines = ['One number is correct and in the right place', 'One number is correct but in the wrong place', 'Two numbers are correct but in the wrong place', 'Nothing is correct', 'One number is correct but in the wrong place'];
 
 var answer1 = "8";
 var answer2 = "2";
 var answer3 = "0";
 
+document.addEventListener('DOMContentLoaded',()=>{
+    setElements();
+});
+
+function setElements(){
+    var buttonDisplay = document.getElementById("buttonDisplay");
+
+    let code = buttonDisplay.innerHTML;
+    console.log(code);
+
+    var count = 0;
+    for(var i=1;i<=5;i++){
+        let temp = `
+        <section class="Section-${i} common">
+        <input type="button" value=${data[count]} onclick="inputHandler(${data[count]})">
+        <input type="button" value=${data[count+1]} onclick="inputHandler(${data[count+1]})">
+        <input type="button" value=${data[count+2]} onclick="inputHandler(${data[count+2]})">
+        <label for="">${dataLines[i-1]}</label>
+        </section>`
+        count += 3
+        code += temp;
+    }
+    buttonDisplay.innerHTML = code;
+}
+
+// onClick functions for display
 function myFunction()
 {
     document.querySelector("#input1").innerHTML = ''
